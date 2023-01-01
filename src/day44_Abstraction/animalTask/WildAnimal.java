b832bd1325be944343e10e62ed3abeea106fe90a
seymaergun <seymaergun89@gmail.com>
2023-01-01 11:01:53 +0100

    day 45_Abstraction and Interface

---
+++ src/day44_Abstraction/animalTask/WildAnimal.java
@@ -0,0 +1,12 @@
+package day44_Abstraction.animalTask;
+
+public interface WildAnimal {
+
+    boolean isWild = true; // static and final by default
+
+    void hunt(); // abstract by default
+
+
+
+
+}
